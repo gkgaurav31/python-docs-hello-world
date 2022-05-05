3ed67d2d9793ac439af2c922e21e5b1b1d224f6c
gkgaurav31 <gkgaurav31@users.noreply.github.com>
2022-05-05 14:55:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask
-import pybdf
-import heartpy as hp
-
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World - GitHub - pybdf!"
+    return "Hello, World - GitHub"
